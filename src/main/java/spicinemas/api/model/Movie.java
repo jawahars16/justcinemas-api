193d45badae655e2ac2e7509b64b145cff5526ad
sumit-kumar14 <sumitgupta.gupta14@gmail.com>
2018-07-19 10:31:57 +0530

    Removed Lambock

--- src/main/java/spicinemas/api/model/Movie.java
+++ src/main/java/spicinemas/api/model/Movie.java
@@ -1,11 +1,9 @@
 package spicinemas.api.model;
 
-import lombok.EqualsAndHashCode;
 import spicinemas.api.type.MovieListingType;
 
 import java.io.Serializable;
 
-@EqualsAndHashCode(exclude = {"id"})
 public class Movie implements Serializable {
     private String title;
     private String poster;
@@ -39,79 +37,19 @@ public class Movie implements Serializable {
         return title;
     }
 
-    public String getPoster() {
-        return poster;
-    }
-
     public String getLanguage() {
         return language;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSynopsis() {
-        return synopsis;
-    }
-
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getActors() {
-        return actors;
-    }
-
-    public void setActors(String actors) {
-        this.actors = actors;
-    }
-
     public MovieListingType getType() {
         return type;
     }
 
-    public void setType(MovieListingType type) {
-        this.type = type;
-    }
-
     public String getLocation() {
         return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getExperience() {
-        return experience;
-    }
-
-    public void setExperience(String experience) {
-        this.experience = experience;
     }
 }
